b989ccb932864cda9765135cee820306906d7666
ysmur <ysmur@yandex.ru>
2025-01-17 17:34:27 +0300

    Звезды на финальном экране (урок 8)

--- Игра в целом/Анимация.py
+++ Игра в целом/Анимация.py
@@ -1,4 +1,5 @@
 import os
+import random
 import sys
 import pygame
 
@@ -24,6 +25,37 @@ class AnimatedSprite(pygame.sprite.Sprite):
         self.cur_frame = (self.cur_frame + 1) % len(self.frames)
         self.image = self.frames[self.cur_frame]
 
+class Particle(pygame.sprite.Sprite):
+    # сгенерируем частицы разного размера
+
+    def __init__(self, pos, dx, dy):
+        super().__init__(app.par_sprites)
+        self.fire = [app.load_image("star.png")]
+        for scale in (5, 10, 20):
+            self.fire.append(pygame.transform.scale(self.fire[0], (scale, scale)))
+        self.image = random.choice(self.fire)
+        self.rect = self.image.get_rect()
+
+        # у каждой частицы своя скорость — это вектор
+        self.velocity = [dx, dy]
+        # и свои координаты
+        self.rect.x, self.rect.y = pos
+
+        # гравитация будет одинаковой (значение константы)
+        self.gravity = 0.1
+
+    def update(self):
+        # применяем гравитационный эффект:
+        # движение с ускорением под действием гравитации
+        self.velocity[1] += self.gravity
+        # перемещаем частицу
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+        # убиваем, если частица ушла за экран
+        if not self.rect.colliderect(app.screen_rect):
+            self.kill()
+
+
 class App:
     def __init__(self):
         pygame.init()
@@ -34,8 +66,10 @@ class App:
         #pygame.key.set_repeat(200, 70)
         self.tile_width = self.tile_height = 50
         self.all_sprites = pygame.sprite.Group()
+        self.par_sprites = pygame.sprite.Group()
         dragon = AnimatedSprite(self, self.load_image("pygame-8-1.png"), 8, 2, 50, 50)
         self.fps = 30
+        self.screen_rect = (0, 0, self.width, self.height)
 
 
     def terminate(self):
@@ -58,32 +92,35 @@ class App:
             image = image.convert_alpha()
         return image
 
-    def start_screen(self):
-        intro_text = ["ЗАСТАВКА", "",
-                      "Правила игры",
-                      "Если в правилах несколько строк,",
-                      "приходится выводить их построчно"]
+    def create_particles(self, position):
+        # количество создаваемых частиц
+        particle_count = 20
+        # возможные скорости
+        numbers = range(-5, 6)
+        for _ in range(particle_count):
+            Particle(position, random.choice(numbers), random.choice(numbers))
+
+    def end_screen(self):
+        intro_text = ["Game over"]
 
         fon = pygame.transform.scale(self.load_image('fon.jpg'), (self.width, self.height))
         self.screen.blit(fon, (0, 0))
         font = pygame.font.Font(None, 30)
         text_coord = 50
-        for line in intro_text:
-            string_rendered = font.render(line, 1, pygame.Color('white'))
-            intro_rect = string_rendered.get_rect()
-            text_coord += 10
-            intro_rect.top = text_coord
-            intro_rect.x = 10
-            text_coord += intro_rect.height
-            self.screen.blit(string_rendered, intro_rect)
+
+        string_rendered = font.render(intro_text[0], 1, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        self.screen.blit(string_rendered, intro_rect)
 
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.terminate()
-                elif event.type == pygame.KEYDOWN or \
-                        event.type == pygame.MOUSEBUTTONDOWN:
-                    return  # начинаем игру
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    # создаём частицы по щелчку мыши
+                    self.create_particles(pygame.mouse.get_pos())
+            self.par_sprites.update()
+            self.par_sprites.draw(self.screen)
             pygame.display.flip()
             self.clock.tick(self.fps)
 
@@ -96,7 +133,8 @@ class App:
                 if event.type == pygame.QUIT:
                     self.terminate()
                 if event.type == pygame.KEYDOWN and event.key == pygame.K_1:
-                    self.game_over += 1
+                    run = False
+                    self.end_screen()
             keys = pygame.key.get_pressed()
             if keys[pygame.K_DOWN]:
                 self.hero.update((0, self.tile_height))
@@ -119,5 +157,4 @@ class App:
 
 if __name__ == '__main__':
     app = App()
-    app.start_screen()
     app.run_game()
